f0274bf3280bd45fb4af168398503836c7d3dcbf
Nilesh Mathur <110603429+nmathur478@users.noreply.github.com>
2023-04-05 17:30:08 +0530

    Created action for listing packages with their tags for an organization (#1)
    * Created action for listing packages with their tags for an organization
    
    * Disabled linter for dist/index.js file
    
    * Disable the linter for dist/index.js file for resolving errors
    
    * Resolved linting errors
    
    * Testing the github action
    
    * Created a token for testing
    
    * Updated dist/index.js file

---
+++ index.js
@@ -0,0 +1,38 @@
+const core = require('@actions/core')
+const Github = require('@actions/github')
+
+const listPackagesWithTags = async () => {
+  try {
+    const versionedPackages = []
+    let packagesArray = core.getInput('image_name', { required: true })
+    packagesArray = packagesArray.split(',').map(x => x.trim())
+    const token = core.getInput('GET_PACKAGES_TOKEN', { required: true })
+    const github = Github.getOctokit(token)
+    const packageType = core.getInput('package_type', { required: true })
+    const organization = core.getInput('organization', { required: true })
+
+    for (let i = 0; i < packagesArray.length; i++) {
+      const resp1 = await github.request('GET /orgs/{organization}/packages/{packageType}/{packageName}/versions', {
+        packageName: packagesArray[i],
+        packageType,
+        organization
+      })
+      const resp2 = resp1.data.map(data => data.metadata)
+      for (let j = 0; j < resp2.length; j++) {
+        const tags = resp2[j].container.tags
+        if (tags === []) {
+          continue
+        } else {
+          for (let k = 0; k < tags.length; k++) {
+            versionedPackages.push(packagesArray[i].concat(':', tags[k]))
+          }
+        }
+      }
+    }
+    console.log(versionedPackages)
+    core.setOutput('packages-with-tags', versionedPackages)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+listPackagesWithTags()
